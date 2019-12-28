4c43e5eacebc8ec6839f8c41cd9c4a99674a37c2
Rafael Correia <2171295@my.ipleiria.pt>
2019-12-28 12:52:48 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div >
+      <ola/>
+      {{this.true}}
+    </div>
+
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import ola from '~/components/ola.vue'
+
+export default {
+  data: function () {
+    return{
+      true: '2'
+
+    }
+
+  },
+  components: {
+    Logo,
+    VuetifyLogo,
+    ola
+  }
+}
+</script>
