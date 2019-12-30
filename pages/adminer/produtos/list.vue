26441dbb9e139792b43c8289a7afe44ad3871912
Rafael Correia <2171295@my.ipleiria.pt>
2019-12-30 02:46:24 +0000

    List Prodtucts is visible and working

--- pages/adminer/produtos/list.vue
+++ pages/adminer/produtos/list.vue
@@ -1,11 +1,65 @@
 <template>
-
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="produtos"
+      :items-per-page="10"
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <tr @click="props.expanded = !props.expanded">
+          <td class="text-xs">{{ props.item.code }}</td>
+          <td class="text-xs">{{ props.item.tipo.nome}}</td>
+          <td class="text-xs">{{ props.item.descricao}}</td>
+          <td class="text-xs">{{ props.item.preco}}</td>
+        </tr>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "list"
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Codigo',
+            align: 'left',
+            sortable: false,
+            value: 'code'
+          },
+          {
+            text: 'Tipo',
+            align: 'left',
+            sortable: false,
+            value: 'tipo.nome'
+          },
+          {
+            text: 'Descrição',
+            align: 'left',
+            sortable: false,
+            value: 'descricao'
+          },
+          {
+            text: 'Preço',
+            align: 'left',
+            sortable: false,
+            value: 'preco'
+          }
+        ],
+        produtos: []
+      }
+    },
+    created () {
+
+      this.$axios.$get('/api/produtos')
+        .then((produtos) => {
+          this.produtos = produtos;
+          console.log(produtos);
+        });
     }
+  }
 </script>
 
 <style scoped>
